9da2d082690cf956cf81a9423e6066f0f1752a1b
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-03-21 18:48:56 +0300

    Add files via upload

---
+++ include/Automata.h
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
+
+class Automata {
+private:
+	int cash;
+	vector<string> menu;
+	vector<int> prices;
+	STATES state;
+public:
+	void on();
+	void off();
+	void coin(int money);
+	string getMenu();
+	STATES getState();
+	void choice(int choice);
+	bool check(int choice);
+	void cancel();
+	void cook();
+	void finish();
+};
