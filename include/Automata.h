32b80827a21e0b993ec8dc54eb835d82055f363f
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-03-21 20:13:38 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -8,20 +8,20 @@ enum STATES { OFF, WAIT, ACCEPT, CHECK, COOK };
 
 class Automata {
 private:
-	int cash;
-	vector<string> menu;
-	vector<int> prices;
-	STATES state;
+        int cash;
+        vector<string> menu;
+        vector<int> prices;
+        STATES state;
 public:
-	Automata();
-	void on();
-	void off();
-	void coin(int money);
-	string getMenu();
-	STATES getState();
-	void choice(int choice);
-	bool check(int choice);
-	void cancel();
-	void cook();
-	void finish();
+        Automata();
+        void on();
+        void off();
+        void coin(int money);
+        string getMenu();
+        STATES getState();
+        void choice(int choice);
+        bool check(int choice);
+        void cancel();
+        void cook();
+        void finish();
 };
