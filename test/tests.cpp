6b0f3d2a02d22095b805beef59655017e1c636ee
Roman Denisov <99988283+Fenridan@users.noreply.github.com>
2023-03-21 19:55:48 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,25 +3,25 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
 
-TEST(test_create) {
+TEST(test_create, test_create) {
     Automata automat;
     EXPECT_EQ(OFF, automat.getState());
 }
 
-TEST(test_on) {
+TEST(test_on, test_on) {
     Automata automat;
     automat.on();
     EXPECT_EQ(WAIT, automat.getState());
 }
 
-TEST(test_off) {
+TEST(test_off, test_off) {
     Automata automat;
     automat.on();
     automat.off();
     EXPECT_EQ(OFF, automat.getState());
 }
 
-TEST(test_coin) {
+TEST(test_coin, test_coin) {
     Automata automat;
     automat.on();
     automat.coin(150);
@@ -29,7 +29,7 @@ TEST(test_coin) {
 }
 
 
-TEST(test_choice) {
+TEST(test_choice, test_choice) {
     Automata automat;
     automat.on();
     automat.coin(100);
@@ -37,7 +37,7 @@ TEST(test_choice) {
     EXPECT_EQ(WAIT, automat.getState());
 }
 
-TEST(test_choice_v2) {
+TEST(test_choice_v2, test_choice_v2) {
     Automata automat;
     automat.on();
     automat.coin(10);
@@ -45,20 +45,20 @@ TEST(test_choice_v2) {
     EXPECT_EQ(WAIT, automat.getState());
 }
 
-TEST(test_cook) {
+TEST(test_cook, test_cook) {
     Automata automat;
     automat.on();
     automat.cook();
     EXPECT_EQ(COOK, automat.getState());
 }
 
-TEST(test_choice) {
+TEST(test_cancel, test_cancel) {
     Automata automat;
     cancel();
     EXPECT_EQ(WAIT, automat.getState());
 }
 
-TEST(test_full_work) {
+TEST(test_full_work, test_full_work) {
     Automata automat;
     automat.on();
     automat.coin(10);
